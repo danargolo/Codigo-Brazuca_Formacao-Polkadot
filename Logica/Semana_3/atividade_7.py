b32076f055e1bdcb02024ee51a905469fc7d8a3b
danargolo <dan.argolo@gmail.com>
2024-08-23 13:12:13 -0300

    fix: erro ao adicionar nota invalida

--- Logica/Semana_3/atividade_7.py
+++ Logica/Semana_3/atividade_7.py
@@ -1,18 +1,13 @@
-#Atividade 7: Média de Notas
-
-""" Desenvolva um programa que calcule a média de várias notas inseridas pelo
-usuário. O programa deve parar de pedir notas quando o usuário digitar -1. """
-
-
 def calculadora_medias():
   notas = []
 
   nota = float(input("Digite uma nota ou -1 para encerrar:\n "))
 
   while nota != -1:
-    if nota < 0 or nota > 10:
-      print("Nota inválida! Digite uma nota entre 0 e 10.")
-      break
+
+    while nota < 0 or nota > 10:
+      nota = float(input("Nota inválida! Digite uma nota de 0 a 10:\n "))
+
     notas.append(nota)
     nota = float(input("Digite uma nota ou -1 para encerrar:\n "))
 
@@ -21,6 +16,6 @@ def calculadora_medias():
   
   media_notas = sum(notas) / len(notas)
 
-  print(f'Foram adicionadas {len(notas)} e a média das notas é: {media_notas:.1f}')
+  print(f"Foram adicionadas {len(notas)} e a média das notas é:{media_notas:.1f}.")
 
 calculadora_medias()
