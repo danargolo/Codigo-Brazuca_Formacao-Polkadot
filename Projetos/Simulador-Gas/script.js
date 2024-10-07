d7e83bc93eb9fa3e957644644c8ded345caa24b4
danargolo <dan.argolo@gmail.com>
2024-10-06 23:31:45 -0300

    commit bomba

--- Projetos/Simulador-Gas/script.js
+++ Projetos/Simulador-Gas/script.js
@@ -1,14 +1,63 @@
+import {fiatCurrencies} from './fiatCurrencies.json';
 let contador = 0;
 let historicoTimeout;
+let temporizadorId;
 
 let divMoeda = document.getElementById('selecao-moeda');
-let moedaButtons = document.getElementsByClassName('moeda-btn');
+let fiat = document.getElementById('seletor-btn')
 let contadorDisplay = document.getElementById('id_transacao');
 let historicoLista = document.getElementById('historico-lista');
 let temporizador = document.getElementById('temporizador');
 const excecao = document.getElementById('excecao');
 const btnCalcular = document.getElementById('calcular');
 const resultado = document.getElementById('resultado');  
+const inputTransacao = document.getElementById('valor-transacao');
+const moedasContainer = document.getElementById('moedas-container');
+// const cachedData = localStorage.getItem(fiat.textContent.trim())
+const tabs = document.getElementsByClassName('tab-btn');
+const tabContents = document.getElementsByClassName('tab-conteudo');
+
+
+let result = document.getElementById('result-transacao');
+let resultComplexidade = document.getElementById('result-complexidade');
+let resultDot = document.getElementById('result-dot-estimado');
+let resultCur = document.getElementById('result_currency')
+let resultMoeda = document.getElementById('result-preco-estimado');
+
+
+async function fetchCryptocurrencies(fiat) {
+  const url = 'http://localhost:5000/dot-exchange';
+  
+  const cachedData = localStorage.getItem(fiat);
+
+  if (cachedData) {
+    console.log('Dados carregados do cache');
+    return JSON.parse(cachedData);
+  }
+  console.log('Chamou na API');
+  
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ fiat })
+  });
+      
+      if (!response.ok) {
+          throw new Error(`Erro: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+
+      localStorage.setItem(fiat, JSON.stringify(data.data));
+
+      return data.data;
+  } catch (error) {
+      console.error('Erro ao buscar dados:', error);
+  }
+}
 
 
 function incrementarContador () {
@@ -17,10 +66,10 @@ function incrementarContador () {
   contadorDisplay.textContent = `# ${contador}`;
 };
 
-function adicionaHistorico (valorTransacao, complexidade) {
+function adicionaHistorico (valor,complexidade, valorTransacao, valorCurrency ) {
 
   let li = document.createElement('li');
-  li.textContent = `Valor: ${valorTransacao} DOT, Complexidade: ${complexidade}, valor gas?`;
+  li.textContent = `Valor: ${valor} Complexidade: ${complexidade}, valor gas dot: ${valorTransacao}, valor gas: ${ valorCurrency}`;
 
   historicoLista.appendChild(li);
 
@@ -48,6 +97,47 @@ function validarValor(value) {
       btnCalcular.disabled = false;
     }
   }
+
+  function renderFiatCurrencies() {
+    
+    moedasContainer.innerHTML = '';    
+
+    fiatCurrencies.forEach(currency => {      
+        const currencyElement = document.createElement('button');
+        currencyElement.classList.add("moeda-btn");
+        currencyElement.textContent = currency.code;
+        moedasContainer.appendChild(currencyElement);
+    });
+}
+
+  function iniciarTemporizador(duracao, display) {
+    let tempoRestante = duracao;
+
+    if (temporizadorId) {
+      return;
+    }
+
+    temporizadorId = setInterval(function () {
+
+      let minutos = Math.floor(tempoRestante / 60);
+      let segundos = tempoRestante % 60;
+
+      minutos = minutos < 10 ? '0' + minutos : minutos;
+      segundos = segundos < 10 ? '0' + segundos : segundos;
+
+      display.textContent = minutos + ":" + segundos;
+
+      if (tempoRestante <= 0) {
+        clearInterval(temporizadorId);
+        temporizadorId = null;
+        display.textContent = "00:00";
+        // iniciarTemporizador(duracao, display)
+        limparHistorico();
+      }
+
+      tempoRestante--;
+    }, 1000);
+  };
   
   document.getElementById('valor-transacao').addEventListener('input', function () {
     validarValor(this.value);
@@ -62,17 +152,17 @@ function validarValor(value) {
   }
 })
 
-for (let i = 0; i < moedaButtons.length; i++) {
-  moedaButtons[i].addEventListener('click', function () {
-    document.getElementById('seletor-btn').textContent = moedaButtons[i].textContent;
-    divMoeda.style.display = 'none';
-  });
-}
-
-document.getElementById('calcular').addEventListener('click', function () {
+document.getElementById('calcular').addEventListener('click', async function () {
+  let valorMoeda = await fetchCryptocurrencies(fiat.textContent.trim());
+  
+  const fiatValue = fiat.textContent.trim();
+  const price = valorMoeda.quote[fiatValue].price;
+  
   // Captura os valores inseridos pelo usuário
-  let valorTransacao = parseFloat(document.getElementById('valor-transacao').value);
-  let complexidade = document.getElementById('complexidade').value;
+  let valorTransacao = document.getElementById('valor-transacao').value;
+
+  let complexidade = document.getElementById('complexidade').value; 
+
   let precoGas;
   // Definição de preço do gas por complexidade
   switch (complexidade) {
@@ -87,44 +177,60 @@ document.getElementById('calcular').addEventListener('click', function () {
       break;
   }
   // Calcula o custo total do gas
-  let custoGas = valorTransacao * precoGas;
-  // Exibe o resultado
-  // if (valorTransacao > 0) {
-  //   document.getElementById('resultado').innerHTML = `
-  // <p>Valor da Transação: ${valorTransacao} DOT</p>
-  // <p>Complexidade: ${complexidade.charAt(0).toUpperCase() +
-  //     complexidade.slice(1)}</p>
-  // <p><strong>Custo Estimado do Gas: ${custoGas.toFixed(2)} DOT</strong></p>
-  // `;
-  // } else {
-  //   document.getElementById('resultado').innerHTML = '<p style="color:red;">Por favor, insira  um valor de transação válido.</p > ';
-  // }
-  document.getElementById('valor-transacao').value = undefined
-  adicionaHistorico(valorTransacao, complexidade)
+  let custoGasDot = (valorTransacao * precoGas).toFixed(2)
+  let custoGasCur = (custoGasDot * price).toFixed(2)
+
+  resultMoeda.textContent = `${fiat.textContent.trim()}: ${custoGasCur}`;
+
+
+  result.textContent = valorTransacao;
+  resultComplexidade.textContent = complexidade;
+  resultDot.textContent = custoGasDot;
+
+  adicionaHistorico(valorTransacao, complexidade, custoGasDot,custoGasCur)
+  document.getElementById('valor-transacao').value = '';
+  document.getElementById('complexidade').value = 'baixa'
   incrementarContador();
+
+  iniciarTemporizador(60, temporizador);
 
   this.disabled = true;
 });
 
 
-function openTab(evt, tabName) {  
+function openTab(evt) {
+  const tabName = evt.currentTarget.getAttribute('data-tab');
 
-  let tabContents = document.getElementsByClassName('tab-conteudo');
-  for (let i = 0; i < tabContents.length; i++) {
-    tabContents[i].style.display = 'none';
+  for (let content of tabContents) {
+    content.style.display = 'none';
   }
 
   document.getElementById(tabName).style.display = 'block';
 
-  let tabButtons = document.getElementsByClassName('tab-btn');
-  for (let i = 0; i < tabButtons.length; i++) {
-    tabButtons[i].className = tabButtons[i].className.replace('active', '');
+  for (let tab of tabs) {
+    tab.className = tab.className.replace(' active', '');
   }
 
-  evt.currentTarget.className += 'active';
+  evt.currentTarget.className += ' active';
+}
+
+for (let tab of tabs) {
+  tab.addEventListener('click', openTab)
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.tab-btn').click();
+  renderFiatCurrencies();
+
+
+  const moedaButtons = document.querySelectorAll('.moeda-btn');
+
+  for (let button of moedaButtons) {
+    button.addEventListener('click', function () {
+      document.getElementById('seletor-btn').textContent = button.textContent;
+      divMoeda.style.display = 'none';
+
+    });
+  }
   
 });
